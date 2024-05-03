78a99e2a89c14a98d7e05bad9bc9e70ea85c7b58
Dominik Velic <xvelic@stuba.sk>
2024-05-03 17:42:10 +0200

    init

---
+++ src/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        html {
+            max-width: 70ch;
+            padding: 3em 1em;
+            margin: auto;
+            line-height: 1.75;
+            font-size: 1.25em;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Init</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae autem sapiente corrupti? Eveniet beatae atque id nulla quaerat sit inventore sed distinctio sapiente. Aliquid quas quisquam placeat, ex sit expedita.</p>
+</body>
+
+</html>
