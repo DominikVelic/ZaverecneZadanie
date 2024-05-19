a652783b1f992aec5fbf42df9d2025dd84f9bd1e
Jezsó Áron <xjezsoa@stuba.sk>
2024-05-19 13:49:03 +0200

    Header Fix

--- src/header.php
+++ src/header.php
@@ -1,22 +1,20 @@
 <?php
 
 session_start();
-require "language_change.php"
+require "language_change.php";
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="/ZaverecneZadanie/src/css/main.css">
 </head>
-
 
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5 p-5">
@@ -28,17 +26,17 @@ require "language_change.php"
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav text-white ms-5">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.php"><?php echo $lang['home_link_text'] ?></a>
+                        <a class="nav-link active" aria-current="page" href="/ZaverecneZadanie/src/index.php"><?php echo $lang['home_link_text'] ?></a>
                     </li>
                 </ul>
                 <ul class="navbar-nav ms-auto">
                     <!-- Login Button -->
                     <li class="nav-item">
-                        <a class="nav-link" href="2FA/login.php"><?php echo $lang['login_text'] ?></a>
+                        <a class="nav-link" href="/ZaverecneZadanie/src/2FA/login.php"><?php echo $lang['login_text'] ?></a>
                     </li>
                     <!-- Register Button -->
                     <li class="nav-item">
-                        <a class="nav-link" href="2FA/register.php"><?php echo $lang['register_text'] ?></a>
+                        <a class="nav-link" href="/ZaverecneZadanie/src/2FA/register.php"><?php echo $lang['register_text'] ?></a>
                     </li>
                     <!-- Language Dropdown -->
                     <li class="nav-item dropdown">
