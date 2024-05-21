4ef83d0bf3acd54e6c620da434aa583ddbce7546
Dominik Velic <125889081+DominikVelic@users.noreply.github.com>
2024-05-21 20:16:54 +0200

    updated date-created to date_created

--- .docker/mysql/sql_scripts/questions.sql
+++ .docker/mysql/sql_scripts/questions.sql
@@ -33,7 +33,7 @@ CREATE TABLE `questions` (
   `subject` varchar(255) NOT NULL,
   `closed` tinyint(1) DEFAULT NULL,
   `code` int NOT NULL,
-  `date-created` date NOT NULL
+  `date_created` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
