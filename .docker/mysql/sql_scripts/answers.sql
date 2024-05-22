bfe2a98ca67f4b53c7009bddee41e1beb11b1389
Dominik Velic <xvelic@stuba.sk>
2024-05-22 19:52:14 +0200

    NULL appearance

--- .docker/mysql/sql_scripts/answers.sql
+++ .docker/mysql/sql_scripts/answers.sql
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `answers` (
   `id` int NOT NULL,
   `answer` varchar(512) NOT NULL,
-  `appearance` varchar(255) NOT NULL,
+  `appearance` varchar(255) DEFAULT NULL,
   `count` int NOT NULL,
   `question_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
